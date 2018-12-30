12109e6f3225125cf739b9db63c78079c3fb005e
Akshat Chopra <akshatchopra321@gmail.com>
2018-12-30 12:41:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ function uploadTheImage()
   var sourceOfPicture = "saadi ugly.png";
   var img = document.getElementById('saadiPic');
   img.src = sourceOfPicture.replace('20x20', '25x25');
-  if (img.style.display == "none")
-  {
-    img.style.display = "block";
-  }
-  else
+  if (img.style.display == "block")
   {
     img.style.display = "none";
   }
+  else
+  {
+    img.style.display = "block";
+  }
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySideNav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySideNav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
