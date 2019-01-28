2c1f39ea7b70bffa17c3b01ea385686b1ef943eb
Akshat Chopra <akshatchopra321@gmail.com>
2019-01-27 22:15:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function uploadTheImage()
 {
-  var sourceOfPicture = "saadi ugly.png";
+  var sourceOfPicture = "linkedIn DC.JPG";
   var img = document.getElementById('saadiPic');
   img.src = sourceOfPicture.replace('20x20', '25x25');
   if (img.style.display == "block")
